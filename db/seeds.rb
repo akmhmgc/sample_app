acbbea79645c0167658583d3c6201607e5be9e21
akmhmgc <akimuhamaguchi@icloud.com>
2021-01-15 13:13:36 +0900

    seed add an account

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@ User.create!(name:  "Example User",
     activated: true,
     activated_at: Time.zone.now)
 
+User.create!(name:  "Akimu Hamaguchi",
+        email: "akimu-hamaguchi@i.softbank.jp",
+        password:              "foobar",
+        password_confirmation: "foobar",
+        admin:     true,
+        activated: true,
+        activated_at: Time.zone.now)
+
+
 99.times do |n|
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
